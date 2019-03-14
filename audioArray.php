d23da2e425c3d0e307dfa989f387d97c46525803
Manuel <manu.wiizzle@gmail.com>
2019-03-14 15:50:07 +0100

    add file php

---
+++ audioArray.php
@@ -0,0 +1,93 @@
+<?php
+$audioArray = [
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh.',
+        'price' => '79.99$',
+        'image' => 'assets/audio1.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod1'
+    ],
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh.',
+        'price' => '89.99$',
+        'image' => 'assets/audio2.png',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod2'
+    ],
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh.',
+        'price' => '59.99$',
+        'image' => 'assets/audio3.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod3'
+    ],
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh.',
+        'price' => '59.99$',
+        'image' => 'assets/audio4.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod4'
+    ],
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh.',
+        'price' => '79.99$',
+        'image' => 'assets/audio5.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod5'
+    ],
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh.',
+        'price' => '29.99$',
+        'image' => 'assets/audio6.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod6'
+    ],
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh.',
+        'price' => '149.99$',
+        'image' => 'assets/audio7.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod7'
+    ],
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh.',
+        'price' => '139.99$',
+        'image' => 'assets/audio8.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod8'
+    ],
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh. ',
+        'price' => '19.99$',
+        'image' => 'assets/audio9.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod9',
+    ],
+    [
+        'title' => 'Wireless Heaphone',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem Elsass ipsum lacus kougelhopf pellentesque ornare id, dolor Mauris libero, suspendisse porta chambon amet Pellentesque leo sit libero, ac sagittis kartoffelsalad Morbi risus, yeuh.',
+        'price' => '19.99$',
+        'image' => 'assets/audio10.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod10'
+    ],
+];
