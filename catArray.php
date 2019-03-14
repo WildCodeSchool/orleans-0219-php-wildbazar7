7193225384442b1653e96ee58bd9e166c3e6fd0e
Angel Rion-Cervi <a.rioncervi@gmail.com>
2019-03-14 09:42:46 +0100

    fixed some element position in css

---
+++ catArray.php
@@ -0,0 +1,93 @@
+<?php
+$phoneCaseArray = [
+    [
+        'title' => 'Abstract',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '25$',
+        'image' => 'assets/pc0.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod1'
+    ],
+    [
+        'title' => 'Card title',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '25$',
+        'image' => 'assets/pc2.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod2'
+    ],
+    [
+        'title' => 'Card title',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '25$',
+        'image' => 'assets/pc3.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod3'
+    ],
+    [
+        'title' => 'Card title',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '25$',
+        'image' => 'assets/pc4.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod4'
+    ],
+    [
+        'title' => 'Card title',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '25$',
+        'image' => 'assets/pc5.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod5'
+    ],
+    [
+        'title' => 'Card title',
+        'description' => 'BBBBEBEEPJDPV%DIJ',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '99$',
+        'image' => 'assets/pc6.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod6'
+    ],
+    [
+        'title' => 'Card title',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '25$',
+        'image' => 'assets/pc7.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod7'
+    ],
+    [
+        'title' => 'Card title',
+        'description' => 'This is lollolololocard with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '25$',
+        'image' => 'assets/pc8.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod8'
+    ],
+    [
+        'title' => 'Card title',
+        'description' => 'This is etherrrra longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '25$',
+        'image' => 'assets/pc9.jpg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod9'
+    ],
+    [
+        'title' => 'Card title',
+        'description' => 'This is a longer card withsbbsdbqfbeneth text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '45$',
+        'image' => 'assets/pc10.jpg',
+        'stock' => 'out of stock',
+        'modalTarget' => 'mod10'
+    ],
+];
