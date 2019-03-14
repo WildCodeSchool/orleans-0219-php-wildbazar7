f3f1e55ddeda10203548f6b176bd8c7de194ed69
foforato <arhim45@gmail.com>
2019-03-14 15:46:36 +0100

    add php in charger&battery

---
+++ pageProduitArray.php
@@ -0,0 +1,93 @@
+<?php
+$pageProduitArray = [
+    [
+        'title' => 'Belkin BOOST Wireless Charging',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '100$',
+        'image' => 'assets/chargeur1.jpeg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod1'
+    ],
+    [
+        'title' => 'Smart Battery Case pour iPhone XS Max',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '67$',
+        'image' => 'assets/chargeur2.jpeg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod2'
+    ],
+    [
+        'title' => 'Neat',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '97$',
+        'image' => 'assets/chargeur3.jpeg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod3'
+    ],
+    [
+        'title' => 'Mophie wireless charging base',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '45$',
+        'image' => 'assets/chargeur4.jpeg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod4'
+    ],
+    [
+        'title' => 'Belkin Car Charger with Built-in Lightning Cable',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '93$',
+        'image' => 'assets/chargeur5.jpeg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod5'
+    ],
+    [
+        'title' => 'Apple Watch Magnetic Charging Dock',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '55$',
+        'image' => 'assets/chargeur6.jpeg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod6'
+    ],
+    [
+        'title' => 'Belkin Travel Charge Kit',
+        'description' => 'This is a longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '45$',
+        'image' => 'assets/chargeur7.jpeg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod7'
+    ],
+    [
+        'title' => 'Logitech Powered',
+        'description' => 'This is lollolololocard with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '65$',
+        'image' => 'assets/chargeur8.jpeg',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod8'
+    ],
+    [
+        'title' => 'Belkin Base',
+        'description' => 'This is etherrrra longer card with supporting text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '78$',
+        'image' => 'assets/chargeur11.png',
+        'stock' => 'in stock',
+        'modalTarget' => 'mod9'
+    ],
+    [
+        'title' => 'Powered Base',
+        'description' => 'This is a longer card withsbbsdbqfbeneth text below as a natural lead-in to',
+        'descriptionModal' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque quis orci ullamcorper consectetur. Donec nunc dolor',
+        'price' => '45$',
+        'image' => 'assets/chargeur10.png',
+        'stock' => 'out of stock',
+        'modalTarget' => 'mod10'
+    ],
+];
